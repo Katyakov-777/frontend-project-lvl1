79840b9718a0105321445fcd61ff348e81dcfbc3
ekaterina iakovleva <katyakov777@mail.ru>
2021-01-08 17:29:57 +0100

    add brain-progression and brain-prime games

---
+++ src/logic-brain-prime.js
@@ -0,0 +1,49 @@
+import readlineSync from 'readline-sync';
+
+//генератор рандомного числа
+function getRandomInt(max, min) {
+  return Math.floor(Math.random() * (max - min)) + min;
+};
+
+//простое ли число
+const isPrimeNumber = (num) => {
+    for (let i = 2; i < num; i ++) {
+        if (num % i === 0) {
+            return false;
+        }
+    }
+    return true;
+};
+
+//проверка правильности ответа
+const isAnswerCorrect = (num, answer) => {
+    if (isPrimeNumber(num) && answer === "yes") {
+      return true;
+    } else if (!isPrimeNumber(num) && answer === "no") {
+        return true;
+    } else {
+        return false;
+    }
+  };
+
+const question = () => {
+  console.log('Welcome to the Brain Games!');
+  const name = readlineSync.question('May I have your name? ');
+  console.log(`Hello, ${name}!`);
+  console.log('Answer "yes" if given number is prime. Otherwise answer "no".');
+  for (let i = 0; i < 3; i += 1) {
+    let num = getRandomInt(16, 2);
+    console.log("Question: " + num);
+    const answer = readlineSync.question('Your answer: ');
+//победа/поражение
+    if (isAnswerCorrect(num, answer)) {
+      console.log('Correct!');
+    } else {
+      console.log("'" + answer + "'" + " is is wrong answer ;(.");
+      console.log("Let's try again, " + name + "!");
+      return;
+    };
+  }
+  console.log(`Congratulations, ${name}!`);
+};
+export { question as default };
---
+++ src/logic-brain-progression.js
@@ -0,0 +1,58 @@
+import readlineSync from 'readline-sync';
+
+const FIRST_NUMBER_IN_SEQUENCE = 0;
+const MAX_VALUE_FOR_SEQUENCE_STEP = 11;
+const MIN_VALUE_FOR_SEQUENCE_STEP = 2;
+const MAX_SEQUENCE_LENGTH = 11;
+const MIN_SEQUENCE_LENGTH = 5;
+
+//генератор рандомного числа
+function getRandomInt(max, min) {
+  return Math.floor(Math.random() * (max - min)) + min;
+};
+
+//рандомная прогрессия
+function generateRidle() {
+    const initialNumber = getRandomInt(101, 1);
+    const arrayLength = getRandomInt(MAX_SEQUENCE_LENGTH, MIN_SEQUENCE_LENGTH);
+    const step = getRandomInt(MAX_VALUE_FOR_SEQUENCE_STEP, MIN_VALUE_FOR_SEQUENCE_STEP);
+    let progression = [initialNumber];
+    for (let i = 1; i < arrayLength; i ++) {
+        const nextNumber = progression[i - 1] + step;
+        progression.push(nextNumber);
+    }
+    return progression;
+};
+
+//проверка правильности ответа
+const isAnswerCorrect = (hiddenNumber, answer) => {
+    if (hiddenNumber === Number.parseInt(answer)) {
+      return true;
+    }
+    return false;
+  };
+
+const question = () => {
+  console.log('Welcome to the Brain Games!');
+  const name = readlineSync.question('May I have your name? ');
+  console.log(`Hello, ${name}!`);
+  console.log('Find the greatest common divisor of given numbers.');
+  for (let i = 0; i < 3; i += 1) {
+    let value = generateRidle();
+    const hiddenPosition = getRandomInt(value.length, FIRST_NUMBER_IN_SEQUENCE);
+    let hiddenNumber = value[hiddenPosition];
+    value[hiddenPosition] = "..";
+    console.log("Question: " + value.join(" "));
+    const answer = readlineSync.question('Your answer: ');
+//победа/поражение
+    if (isAnswerCorrect(hiddenNumber, answer)) {
+      console.log('Correct!');
+    } else {
+      console.log("'" + answer + "'" + " is is wrong answer ;(. Correct answer was " + "'" + hiddenNumber + "'.");
+      console.log("Let's try again, " + name + "!");
+      return;
+    };
+  }
+  console.log(`Congratulations, ${name}!`);
+};
+export { question as default };
